e0cfb052a82c37d3904fb0b00174c13586667501
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2021-04-07 17:58:42 +0200

    add animation to header objects

--- landingPages/comming-soon/ar/js/index.js
+++ landingPages/comming-soon/ar/js/index.js
@@ -5,14 +5,23 @@ $(document).ready(function() {
         austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
         $('#defaultcountdown').countdown({ until: austDay, format: 'odHMS' });
     });
+    //wow-activating
+    var wow = new WOW({
+
+        mobile: false
+    });
+    wow.init();
+
+
+    //form validation 
     $('.form-control').blur(function() {
-        var x = document.forms["myname"]["email"].value;
+        var x = document.forms["myForm"]["email"].value;
         var atpos = x.indexOf("@");
         var dotpos = x.lastIndexOf(".");
         if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
-            $(this).parent().find(".alert.alert-danger").fadeIn(500);
+            $(this).parent().find(".alert.alert-danger").fadeIn(200);
         } else {
-            $(this).parent().find(".alert.alert-success").fadeIn(500);
+            $(this).parent().find(".alert.alert-success").fadeIn(200);
         }
     });
 });
