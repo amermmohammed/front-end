d3d09f0fe968d422017f3af94eab468e6915dc0d
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2021-04-07 16:04:36 +0200

    50% of app comming soon page

---
+++ landingPages/comming-soon/ar/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // Count-Down
+    $(function() {
+        var austDay = new Date();
+        austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
+        $('#defaultcountdown').countdown({ until: austDay, format: 'odHMS' });
+    });
+    $('.form-control').blur(function() {
+        var x = document.forms["myname"]["email"].value;
+        var atpos = x.indexOf("@");
+        var dotpos = x.lastIndexOf(".");
+        if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+            $(this).parent().find(".alert.alert-danger").fadeIn(500);
+        } else {
+            $(this).parent().find(".alert.alert-success").fadeIn(500);
+        }
+    });
+});
