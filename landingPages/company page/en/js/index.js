592bc0fa9eecf2b3f84b64e06f0fec9540220464
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2021-04-11 17:57:05 +0200

    before getting collapse

--- landingPages/company page/en/js/index.js
+++ landingPages/company page/en/js/index.js
@@ -14,11 +14,4 @@ $(document).ready(function() {
         $('html, body').animate({ scrollTop: $(hash).offset().top - 100 }, 800, function() {});
 
     });
-
-    $('.timer').countTo();
-
-    $(function() {
-        $("#commentForm").validate();
-    });
-
 });
