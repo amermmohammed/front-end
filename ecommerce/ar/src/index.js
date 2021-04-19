6c1fe3287759d2c4d9c958d83f3a19dfdff20c19
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2021-04-19 17:12:02 +0200

    install fontawesome

--- ecommerce/ar/src/index.js
+++ ecommerce/ar/src/index.js
@@ -1,5 +1,6 @@
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import 'bootstrap';
--- ecommerce/en/src/index.js
+++ ecommerce/en/src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import 'bootstrap';
