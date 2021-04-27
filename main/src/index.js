cfb40193a2955e6660e32dd2bc2745f7f82c8e94
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2021-04-27 11:26:16 +0200

    portfolio 75%

---
+++ main/src/index.js
@@ -0,0 +1,56 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min';
+import 'webpack-jquery-ui';
+import 'webpack-jquery-ui/css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap';
+import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min.js';
+
+$(document).ready(function() {
+    $('#progressbar  p span').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 1500,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    $('.count1').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+    $('.count2').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now) + '+');
+            }
+        });
+    });
+    $('.count3').each(function() {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function(now) {
+                $(this).text(Math.ceil(now) + '%');
+            }
+        });
+    });
+});
