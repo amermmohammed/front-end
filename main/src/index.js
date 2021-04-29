c367cee4cf9883f4472082989d5419b321d704ce
Amer Malik Mohammed <amermalikmohammed@gmail.com>
2021-04-29 21:53:15 +0200

    optimizing Portfolio

--- main/src/index.js
+++ main/src/index.js
@@ -6,9 +6,10 @@ import 'webpack-jquery-ui/css';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import 'bootstrap';
-import 'animate.css/animate.min.css';
 import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min.js';
-import 'wow.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 
 
 $(window).scroll(function() {
@@ -17,18 +18,41 @@ $(window).scroll(function() {
     } else {
         $("#navBar").removeClass("noTransparrent");
     }
+    if ($(this).scrollTop() >= 350) {
+        $("#progress1").addClass("progress-bar1");
+    } else {
+        $("#progress1").removeClass("progress-bar1");
+    }
+    if ($(this).scrollTop() >= 350) {
+        $("#progress2").addClass("progress-bar2");
+    } else {
+        $("#progress2").removeClass("progress-bar2");
+    }
+    if ($(this).scrollTop() >= 350) {
+        $("#progress3").addClass("progress-bar3");
+    } else {
+        $("#progress3").removeClass("progress-bar3");
+    }
+    if ($(this).scrollTop() >= 350) {
+        $("#progress4").addClass("progress-bar4");
+    } else {
+        $("#progress4").removeClass("progress-bar4");
+    }
 });
 $(document).ready(function() {
-
+    AOS.init({
+        duration: 800,
+    });
     $("a.scroll").on('click', function(event) {
         var hash = this.hash;
         $('html, body').animate({ scrollTop: $(hash).offset().top - 100 }, 800, function() {});
     });
+
     $('#progressbar  p span').each(function() {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
-            duration: 1500,
+            duration: 1000,
             easing: 'swing',
             step: function(now) {
                 $(this).text(Math.ceil(now));
@@ -39,7 +63,7 @@ $(document).ready(function() {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
-            duration: 4000,
+            duration: 1000,
             easing: 'swing',
             step: function(now) {
                 $(this).text(Math.ceil(now));
@@ -50,7 +74,7 @@ $(document).ready(function() {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
-            duration: 4000,
+            duration: 1000,
             easing: 'swing',
             step: function(now) {
                 $(this).text(Math.ceil(now) + '+');
@@ -61,7 +85,7 @@ $(document).ready(function() {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
         }, {
-            duration: 4000,
+            duration: 1000,
             easing: 'swing',
             step: function(now) {
                 $(this).text(Math.ceil(now) + '%');
@@ -69,10 +93,6 @@ $(document).ready(function() {
         });
     });
 
-    $('[data-toggle="tooltip"]').tooltip();
-    $('.add-to-cart-btn').click(function() {
-        alert('The product has been added to the cart');
-    });
     $('#copyright').text("All rights reserved" + " " + new Date().getFullYear());
 
 
